7c66e89a19c4c04e5e613a727dcfd680994693e4
_piyush <adorably2017@gmail.com>
2023-08-29 14:24:17 +0530

    customer checkin,customer entity update
    customer checkin, with functions

--- src/main/java/controllers/CustomerController.java
+++ src/main/java/controllers/CustomerController.java
@@ -4,10 +4,66 @@
  */
 package controllers;
 
-/**
- *
- * @author shashisu
- */
+import java.sql.SQLException;
+import java.text.ParseException ;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.JOptionPane;
+import java.lang.Object;
+import java.time.LocalDate;
+import java.time.Duration;
+
+import java.text.SimpleDateFormat;
+
 public class CustomerController {
+
+    private final Sql sql;
     
+    public CustomerController() {
+        this.sql = Sql.getInstance();
+    }
+    
+    public void registerCustomer(int id, String customerName, long mobile, String nationality, String gender,String email, String idProof, String idNumber,  String address) {
+        Map<String, Object> customer = new HashMap<>();
+        customer.put("customer_name", (Object) customerName);
+        customer.put("mobile_number", (Object) mobile);
+        customer.put("nationality", (Object) nationality);
+        customer.put("gender", (Object) gender);
+        
+         customer.put("email", (Object) email);
+         
+       customer.put("id_proof", (Object) idProof);
+       customer.put("id_number", (Object) idNumber);
+       customer.put("address", (Object) address);      
+        try {
+             sql.insertInto("customers", customer);
+            JOptionPane.showMessageDialog(null, "Customer created successfully");
+        }
+        catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+    public void customerCheckIn(int customerID,int roomNo,String checkIN, String checkOUT, int price) throws ParseException{
+    LocalDate checkINDate = LocalDate.parse(checkIN);
+    LocalDate checkOUTDate = LocalDate.parse(checkOUT);
+    Duration diff = Duration.between(checkINDate, checkOUTDate);
+    long diffDays = diff.toDays();
+    int stayPeriod= (int) diffDays;//put logic to calculate in days
+    int totalAmount = stayPeriod * price; //by default pass through 
+     Map<String, Object> booking = new HashMap<>();
+        booking.put("customer_id", (Object) customerID);
+        booking.put("check_in", (Object) checkIN);
+        booking.put("check_out", (Object) checkOUT);
+        booking.put("room_no", (Object) roomNo );
+        booking.put("stay_period", (Object)stayPeriod);
+        booking.put("total_amount", (Object) totalAmount);
+          
+        try {
+             sql.insertInto("bookings", booking);
+            JOptionPane.showMessageDialog(null, "Booking done successfully");
+        }
+        catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
 }
--- src/main/java/entities/Customer.java
+++ src/main/java/entities/Customer.java
@@ -1,6 +1,105 @@
 package entities;
 
 
+import java.util.Date;
+
 public class Customer {
     
+    private final int id;
+    private String firstName;
+    private String lastName;
+    
+    private String nationality;
+    
+    private String address;
+    
+    private long mobile;
+    
+    private final String email;
+
+    private String idProof;
+    private String idNumber;
+
+    
+    public Customer(){
+    }
+    public Customer(int id, String firstName, String lastName, String nationality, String address, long mobile, String email, String idProof, String idNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nationality = nationality;
+        this.address = address;
+        this.mobile = mobile;
+        this.email = email;
+        this.idProof = idProof;
+        this.idNumber = idNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public long getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(long mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getIdProof() {
+        return idProof;
+    }
+
+    public void setIdProof(String idProof) {
+        this.idProof = idProof;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+    
+   
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", mobile=" + mobile +
+                '}';
+    }
 }
